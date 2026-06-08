1afe2c08eef7b8b2329ea378208fb1c629243bfd
L-enzo <rodriguezleonelenzo@gmail.com>
2026-06-08 20:53:40 -0300

    correcciones y menu como funcion
    - Se implementó menú como funcion
    - Se corrigió la función de mostrarpilotosypuntos.
    -Se corrigió un condicional de la funcion crearlotepilotos.
    -Se agregó la estructura tEstadisticaPiloto

--- main.c
+++ main.c
@@ -1,48 +1,11 @@
-#include "pilotos.h"
-#include "escuderias.h"
-#include "carreras.h"
-
+#include "menu.h"
 int main(int argc, char* argv[])
 {
-    //menu
-    int estado;
-    do{
-        printf("\n");
-        printf("0. Salir\n");
-        printf("1. Listar pilotos y sus puntos\n");
-        printf("2. Registrar una carrera (ingreso de posiciones)\n");
-        printf("3. Calcular y actualizar puntos automaticamente\n");
-        printf("4. Mostrar ranking de pilotos (ordenado) de la temporada en cuestion\n");
-        printf("5. Mostrar pilotos por escuderia\n");
-        printf("6. Calcular estadisticas de pilotos\n");
-        printf("7. Exportar datos a archivos de texto\n");
-        printf("Ingrese opcion: ");
-        scanf("%d", &estado);
-        printf("\n");
-        switch (estado)
-        {
-            case 0:
-                break;
-            case 1:
-                listarPilotosyPuntos("pilotos.bin");
-                break;
-            case 2:
-                registrarCarrera("carreras.dat");
-                //verificarCarrerasDat("carreras.dat");
-                break;
-            case 3:
-                break;
-            case 4:
-                mostrarRankingPilotos("pilotos.bin", "escuderias.bin");
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-        }
-    }while(estado);
+    //crearLotePilotos("pilotos.txt");
+    //pilotosABin("pilotos.bin","pilotos.txt");
+    //crearLoteEscuderias("escuderias.txt");
+    //escuderiasABin("escuderias.bin","escuderias.txt");
 
+    menu();
     return 0;
 }
